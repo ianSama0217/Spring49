d50107dd2ba9e3fb05a675655df1c945889e015e
ianSama0217 <ian20000217@gmail.com>
2025-12-24 00:09:51 +0800

    init

---
+++ src/main/java/com/example/spring49/Spring49Application.java
@@ -0,0 +1,13 @@
+package com.example.spring49;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring49Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring49Application.class, args);
+		System.out.println("Hello Spring49!!!");
+	}
+}
