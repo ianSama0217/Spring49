1c06c9fd1e06bdd7472c60bb3d6b4fb9ebe45918
ianSama0217 <ian20000217@gmail.com>
2026-01-27 21:56:13 +0800

    重用切入點表達式

--- src/main/java/com/example/spring49/Spring49Application.java
+++ src/main/java/com/example/spring49/Spring49Application.java
@@ -1,13 +1,15 @@
 package com.example.spring49;
 
+import jakarta.annotation.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Spring49Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring49Application.class, args);
-		System.out.println("Hello Spring49!!!");
+		ConfigurableApplicationContext context = SpringApplication.run(Spring49Application.class, args);
+		context.close();
 	}
 }
