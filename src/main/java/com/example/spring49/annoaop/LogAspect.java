f3ab04e2ec229b5d7a4f864274ed3d6c6d1a4a80
ianSama0217 <ian20000217@gmail.com>
2026-01-27 21:57:45 +0800

    重用切入點表達式

--- src/main/java/com/example/spring49/annoaop/LogAspect.java
+++ src/main/java/com/example/spring49/annoaop/LogAspect.java
@@ -10,36 +10,40 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LogAspect {
+    // 重用切入點表達式
+    @Pointcut(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))")
+    private void pointCut(){}
+
     /* 設定切入點和通知類型
      * 通知類型: @Before, @AfterReturning, @AfterThrowing, @After, @Around
      */
 
-    @Before(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))")
+    @Before(value = "pointCut()")
     public void beforeMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
         System.out.println("[Before][日誌]方法名稱: " + methodName + ", 參數: " + Arrays.toString(args));
     }
 
-    @AfterReturning(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))", returning = "result")
+    @AfterReturning(value = "pointCut()", returning = "result")
     public void afterReturningMethod(JoinPoint joinPoint, Object result){
         String methodName = joinPoint.getSignature().getName();
         System.out.println("[AfterReturning][日誌]方法名稱: " + methodName + ", 結果: " + result);
     }
 
-    @AfterThrowing(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))", throwing = "ex")
+    @AfterThrowing(value = "pointCut()", throwing = "ex")
     public void afterThrowingMethod(JoinPoint joinPoint, Throwable ex){
         String methodName = joinPoint.getSignature().getName();
         System.out.println("[AfterThrowing][日誌]方法名稱: " + methodName + ", 異常: " + ex);
     }
 
-    @After(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))")
+    @After(value = "pointCut()")
     public void afterMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         System.out.println("[After][日誌]方法名稱: " + methodName);
     }
 
-    @Around(value = "execution(public int com.example.spring49.annoaop.CalculatorImpl.*(..))")
+    @Around(value = "pointCut()")
     public int aroundMethod(ProceedingJoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
